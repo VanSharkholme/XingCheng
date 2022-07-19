3939449d74e2c9153ffd08cc902eda3ea713b015
VanSharkholme <1033620464@qq.com>
2022-07-19 18:36:20 +0800

    init commit

--- XingCheng/urls.py
+++ XingCheng/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import RedirectView
 from main import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('crawler/', include('crawler.urls')),
 ]
